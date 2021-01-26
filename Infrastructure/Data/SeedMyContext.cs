507d52342c580e1b1788c0a44bdf897f754ee1d4
YunaW-dev <yunaw.dev@gmail.com>
2021-01-25 22:48:03 -0500

    Update SeedMyContext.cs
    added code to seed db initially

--- Infrastructure/Data/SeedMyContext.cs
+++ Infrastructure/Data/SeedMyContext.cs
@@ -1,15 +1,59 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
 {
     public class SeedMyContext
     {
-        public static async Task SeedAsync(MyContext context, ILoggerFactory loggerFactory){
-            try {
+        public static async Task SeedAsync(MyContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProjectYears.Any())
+                {
+                    var yearsFile = File.ReadAllText("../Infrastructure/Data/SeedingData/years.json");
+                    var years = JsonSerializer.Deserialize<List<ProjectYear>>(yearsFile);
+                    foreach (var item in years)
+                    {
+                        context.ProjectYears.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
 
-            }catch(Exception ex){
-                
+                if (!context.ProjectTypes.Any())
+                {
+                    var typesFile = File.ReadAllText("../Infrastructure/Data/SeedingData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProjectType>>(typesFile);
+                    foreach (var item in types)
+                    {
+                        context.ProjectTypes.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+
+                if (!context.Projects.Any())
+                {
+                    var projectsFile = File.ReadAllText("../Infrastructure/Data/SeedingData/projects.json");
+                    var projects = JsonSerializer.Deserialize<List<Project>>(projectsFile);
+                    foreach (var item in projects)
+                    {
+                        context.Projects.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
             }
         }
     }
