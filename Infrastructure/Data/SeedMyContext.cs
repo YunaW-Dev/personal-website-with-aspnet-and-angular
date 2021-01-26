8a29dce48dbbed2c163ddaff5b85dfe2011868f2
YunaW-dev <yunaw.dev@gmail.com>
2021-01-25 21:27:05 -0500

    included some project jsons, seeding them in a moment

---
+++ Infrastructure/Data/SeedMyContext.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class SeedMyContext
+    {
+        public static async Task SeedAsync(MyContext context, ILoggerFactory loggerFactory){
+            try {
+
+            }catch(Exception ex){
+                
+            }
+        }
+    }
+}
